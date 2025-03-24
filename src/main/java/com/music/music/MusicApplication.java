36a759d0fbf5b8f809ad0b7cf5371f09954ce2f4
Piter Rivero <rivero@strands.com>
2025-03-24 12:45:26 +0100

    some change

--- src/main/java/com/music/music/MusicApplication.java
+++ src/main/java/com/music/music/MusicApplication.java
@@ -2,12 +2,19 @@ package com.music.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MusicApplication {
+public class MusicApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MusicApplication.class);
 	}
 
 }
